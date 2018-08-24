5f224a4f44d3c340247908ea2d43fc005c4a49e4
root <root@ndkeren.com>
2018-08-24 11:07:00 +0000

    pkg + test

---
+++ test/test.js
@@ -0,0 +1,47 @@
+var expect = require('chai').expect;
+var debt   = require('../debt');
+
+describe('debt.add()', function () {
+  it('should add debt record', function () {
+	  
+    // 1. ARRANGE
+	// Set something
+
+    // 2. ACT
+    // Do something
+
+    // 3. ASSERT
+    expect(1).to.be.equal(1);
+
+  });
+});
+
+describe('debt.read()', function () {
+  it('should read debt info', function () {
+    
+	// 1. ARRANGE
+	// Set something
+
+    // 2. ACT
+    // Do something
+
+    // 3. ASSERT
+    expect(1).to.be.equal(1);
+
+  });
+});
+
+describe('debt.settle()', function () {
+  it('should try to settle all debts between people', function () {
+    
+	// 1. ARRANGE
+	// Set something
+
+    // 2. ACT
+    // Do something
+
+    // 3. ASSERT
+    expect(1).to.be.equal(1);
+
+  });
+});
