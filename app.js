2e13412fd00143f8c736f2189c2688950a92f62d
root <root@ndkeren.com>
2018-08-24 11:34:11 +0000

    comments bump

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app 	= express();
-var routes  = require('./routes');
+// Init
+var express	= require('express');
+var app		= express();
+var routes	= require('./routes');
 
+// Start
 app.use('/', routes);
 app.listen(8080);
--- debt.js
+++ debt.js
@@ -1,22 +1,46 @@
-// TODO::
-// Debt model should be an object with easier lookup?
-
-
 module.exports = {
 	
+	init: function() {
+		// TODO:: Persist data in file or DB
+		// TODO:: Read at this stage..
+	},
+	append: function() {
+		// TODO:: Add record to file or DB
+	},
 	data: [],
 	// Add a debt
 	add: function(from, to, amount) {
 		
 		// TODO:: Search and aggregate debts..
+		if(!from || !to || !parseFloat(amount)) {
+			return false;
+		}
 		
-		this.data.push([ from , to , amount ]);
-		return from + ' ' + to + ' ' + amount;
+		this.data.push([ from , to , amount, new Date().getTime() ]);
+		
+		// TODO:: More readable format perhaps?
+		// Debt model should be an object with easier lookup?
+		/*
+		this.data.push({
+			from: from,
+			to: to,
+			amount: amount,
+			time: new Date().getTime()
+		});
+		*/
+		
+		return true;
 		
 	},
 	
 	// Read single
 	read: function(name) {
+		
+		
+		if(!name) {
+			// No person specified
+			return false;
+		}
 		
 		var res_in  = [];
 		var res_out = [];
@@ -37,12 +61,21 @@ module.exports = {
 			}				
 		}
 		
+		if(!res_in.length && !res_out.length) {
+			// Specified person not found
+			return false;
+		}
+		
 		return [ res_in, res_out ];
 		
 	},
-	
-	// Settle between all
+	// Get settle solution between all people
 	settle: function() {
+		
+		// TODO::
+		// Iterate the data to merge debts..
+		// Recurcively?
+		// First sort?
 		
 	}
 	
